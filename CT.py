2d926cfb31d91351f9bacbc75d1972d430f8f3dd
borissiu <41263636+borissiu@users.noreply.github.com>
2024-01-05 22:55:31 +0800

    Create CT.py

---
+++ CT.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup 
+
+html = requests.get('https://bgp.he.net/AS4134')
+soup = BeautifulSoup(html.text, 'html.parser')
+
+print('class-list "cl_ISP_CT_v6" ipv6 file')
+for row in soup.find('table', id = "table_prefixes6").tbody.findAll('tr'):
+  columns = row.find_all('td')
+  for td in columns:
+    if td.a:
+      print(td.a.contents[0])
