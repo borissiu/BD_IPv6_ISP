bfefa5ecba2f3f6d3e26f8325fdc2f68efb20a45
borissiu <bsiu@a10networks.com>
2024-01-06 10:02:11 +0800

    Update

---
+++ 4Any.py
@@ -0,0 +1,14 @@
+import sys
+import requests
+from bs4 import BeautifulSoup 
+
+url = 'https://bgp.he.net/' + sys.argv[1]
+html = requests.get(url)
+soup = BeautifulSoup(html.text, 'html.parser')
+
+# print('class-list "cl_ISP_AnyAS_v4" ipv6 file')
+for row in soup.find('table', id = "table_prefixes4").tbody.findAll('tr'):
+  columns = row.find_all('td')
+  for td in columns:
+    if td.a:
+      print(td.a.contents[0])
