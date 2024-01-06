d9238895f93a3f18bba278614db9a7a41adcf55d
borissiu <bsiu@a10networks.com>
2024-01-06 23:05:34 +0800

    Update

--- CM_v4.py
+++ CM_v4.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup 
 
-CM_List = ['AS9808', 'AS56048', 'AS56040', 'AS56046', 'AS56041', 'AS140895', 'AS56047', 'AS56044', 'AS56042', 'AS134810']
+# CM_List = ['AS9808', 'AS56048', 'AS56040', 'AS56046', 'AS56041', 'AS140895', 'AS56047', 'AS56044', 'AS56042', 'AS134810']
+CM_List = ['AS9808']
 print('class-list "cl_ISP_CM_v4" ipv4 file')
 
 for item in CM_List:
